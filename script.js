c1f2b60bae28161ebb85c32a171e1226ab4ee9e9
amutones <aurgena@gmail.com>
2024-09-30 10:55:21 -0500

    Added function to remove old grid before replacing with new grid  Changes to be committed: 	modified:   script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const gridContainer = document.querySelector("#container");
+const body = document.querySelector("body");
 const MAX_GRID_SIZE = 960;
 const gridSize = 16; //starting test size
-// const totalGrids = gridSize ** 2;
-// console.log(totalGrids);
-// const square = document.createElement("div");
-// square.classList.add("square");
-// gridContainer.appendChild(square);
+const gridContainer = document.createElement("div");
+gridContainer.id = "container";
 
 createGrid(gridSize);
 
 function createGrid(gridNum) {
+
+    
     let squareSize = MAX_GRID_SIZE / gridNum;
     squareSize = squareSize - 4;
     let length = squareSize + "px"
@@ -17,12 +16,12 @@ function createGrid(gridNum) {
         const square = document.createElement("div");
         square.classList.add("square");
         square.style.height = length;
-        console.log(length);
         square.style.width = length;
         square.addEventListener("mouseover", event => {
             event.target.style.backgroundColor = "darkgray";
         });
         gridContainer.appendChild(square);
+        body.appendChild(gridContainer);
         
     }
     
@@ -31,8 +30,17 @@ function createGrid(gridNum) {
 const button = document.querySelector("button");
 
 button.addEventListener("click", event => {
+    removeGrid();
+    let size = Number(prompt("How many squares per side?"));
+
     
-    createGrid(16)
+    createGrid(size);
 });
+
+function removeGrid() {
+    while(gridContainer.lastElementChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+}
 
 
