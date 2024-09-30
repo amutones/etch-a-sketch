a3da1904c2de7bfedd02423542204304f704611e
amutones <aurgena@gmail.com>
2024-09-30 10:22:20 -0500

    Added calculation to auto adjust square size  Changes to be committed: 	modified:   script.js

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 const gridContainer = document.querySelector("#container");
-
-const gridSize = 16;
-const totalGrids = gridSize ** 2;
-console.log(totalGrids);
+const MAX_GRID_SIZE = 960;
+const gridSize = 16; //starting test size
+// const totalGrids = gridSize ** 2;
+// console.log(totalGrids);
 // const square = document.createElement("div");
 // square.classList.add("square");
 // gridContainer.appendChild(square);
 
-for (let index = 0; index < totalGrids; index++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.addEventListener("mouseover", event => {
-        event.target.style.backgroundColor = "blue";
-    });
-    gridContainer.appendChild(square);
+createGrid(gridSize);
+
+function createGrid(gridNum) {
+    let squareSize = MAX_GRID_SIZE / gridNum;
+    squareSize = squareSize - 4;
+    let length = squareSize + "px"
+    for (let index = 0; index < gridNum ** 2; index++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.height = length;
+        console.log(length);
+        square.style.width = length;
+        square.addEventListener("mouseover", event => {
+            event.target.style.backgroundColor = "darkgray";
+        });
+        gridContainer.appendChild(square);
+        
+    }
     
 }
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", event => {
+    
+    createGrid(16)
+});
 
 
